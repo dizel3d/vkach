174309bf3688d3c67a53470ac958ec4574a3f1eb
Valentine Sarychev <dizel3d@gmail.com>
2011-10-01 06:15:49 +0400

    Audio detecting

--- vkach.js
+++ vkach.js
@@ -15,38 +15,96 @@
 })
 
 (function() {
-	var createDock = function(default_dock_height, expanded_dock_height) {
-		// create dock
-		$('body').append('<div id="vkach"></div>');
-		var body = $('#page_wrap');
-		var dock = $('#vkach');
-		var easing_type = 'linear';
-		var body_height = $(window).height() - default_dock_height;
-		body.css({'height': body_height, "overflow": "auto"});
-		dock.css({'height': default_dock_height, 'position':'absolute', 'top': body_height, "background": "#ccc", "z-index": 100, "width": "100%"});
+	// class for detecting new audio HTML elements
+	var Audio = function(searchAudioFunction) {
+		this.getAll = searchAudioFunction;
+		this.removingCount = 0;
+	}
+	Audio.prototype = {
+		update: function(newAudioCallback) {
+			var t = this;
+			var auAll = t.getAll();
 
-		// window resize event
-		$(window).resize(function () {
-			updated_height = $(window).height() - default_dock_height;
-			body.css('height', updated_height); 
-			dock.css({'top': updated_height});
-		});
+			// if some elements are being removed now
+			if (t.removingCount) {
+				return;
+			}
 
-		// dock mouse events
-		dock.mouseover(function () {
-			expanded_height = $(window).height() - expanded_dock_height;
-			$(this).animate({'height':expanded_dock_height,'top': expanded_height}, {queue:false, duration:'fast', easing: easing_type});
-		}).mouseout(function () {
-			body_height = $(window).height() - default_dock_height;
-			$(this).animate({'height':default_dock_height,'top': body_height}, {queue:false, duration:'fast', easing: easing_type});
-		});
+			// if audio list was refreshed
+			if (t.first !== auAll.get(0)) {
+				//alert("refresh " + auAll.size());
+				t.first = auAll.get(0);
+				t.index = 0;
+			}
 
-		return dock;
+			// try to get new audios
+			var auNew = auAll.filter(function(i){return(i>=t.index)});
+			if (auNew.size()) {
+
+				// on "remove audio" event
+				auNew.children('.remove_wrap').one('click', function() {
+					++t.removingCount;
+					var removing = $(this).parent();
+					var parent = removing.parent();
+
+					// wait while the element is being removed
+					var wait = function() {
+						if (parent.find(removing).size()) {
+							return setTimeout(wait, 1);
+						}
+
+						// fix fields after removing
+						if (removing.get(0) === t.first) {
+							t.first = parent.children().get(0);
+						}
+						--t.index;
+						--t.removingCount;
+					}
+					wait();
+				});
+
+				// add new audios
+				t.index += auNew.size();
+				newAudioCallback(auNew);
+			}
+		},
 	}
 
-	$(document).ready(function() {
-		dock = createDock(40, 200);
-		dock.append('<a href="https://github.com/dizel3d/">https://github.com/dizel3d/</a>');
-	});
+	var audioDetectors = [
+		new Audio(function() {
+			// get element from all except the audio edit page
+			return $('#audio_create_album').size() ? $() :
+				$('#audios_list #initial_list').children();
+		}),
+		new Audio(function() {
+			// get element from all except the audio edit page
+			return $('#audio_create_album').size() ? $() :
+				$('#audios_list #search_list').children();
+		})
+	];
+
+	var newAudiosProcessor = function(audios) {
+		// test for duplicates
+		var au = audios.filter('[metka="true"]');
+		if (au.size()) {
+			alert('dup ' + au.size());
+		}
+		audios.attr('metka', 'true');
+
+		audios.hover(function() {
+			$(this).find('.duration').css('color', '#cc0000');
+		}, function() {
+			$(this).find('.duration').css('color', '');
+		});
+	};
+
+	setInterval(function() {
+		var start = new Date();
+		for (i in audioDetectors) {
+			audioDetectors[i].update(newAudiosProcessor);
+		}
+		var end = new Date();
+		$('#stl_text').css('opacity', '1').html('Speed ' + (end.getTime()-start.getTime()) + ' ms');
+	}, 200);
 });
 
