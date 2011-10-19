184f20e845ad66335cd927596add00242c8e7e1d
Valentine Sarychev <dizel3d@gmail.com>
2011-10-20 03:22:41 +0400

    jQuery delegation mechanism.

--- vkach.js
+++ vkach.js
@@ -16,150 +16,30 @@
 })
 
 (function() {
-	/* Returns point (x,y) position relative to the first element
-	in the set of matched elements:
-	-2 - left, -1 - above, 0 - inside, 1 - below, 2 - right,
-	null in otherwise (e.g if the set of matched elements is empty). */
-	jQuery.fn.getRelativePosition = function(x, y) {
-		var offset = this.offset();
-		if (!offset)
-			return null;
-		if (y < offset.top)
-			return -1;
-		if (y > offset.top + this.outerHeight())
-			return 1;
-		if (x < offset.left)
-			return -2;
-		if (x > offset.left + this.outerWidth())
-			return 2;
-		return 0;
-	}
-
-	/* Parameters of constructor is a sequence of selectors
-	in which areas audio elements are searched. */
-	var Audio = function() {
-		var self = this;
-		this.searchAreas = arguments;
-		this.clickListeners = [];
-
-		$(document).click(function(e) {
-			var elem = self.getFromPoint(e.pageX, e.pageY);
-			if (elem !== null) {
-				for (var i in self.clickListeners) {
-					self.clickListeners[i].apply(elem, arguments);
-				}
+	panel = {
+		get: function() {
+			var panel = $('#vkach');
+			if (!panel.size()) {
+				$('#header').append('<div id="vkach" style="display: none; border-top: 1px dashed #D9E0E7; padding: 8px 0px 0px; margin: 8px 0px 0px;"></div>');
 			}
-		});
-	}
-	Audio.prototype = {
-		click: function(listener) {
-			this.clickListeners.push(listener);
-			return this;
-		},
-
-		debug: function(on) {
-			if (!on) {
-				delete this.getFromPoint;
-				return this;
-			}
-
-			var count = 0;
-			var sumTime = 0;
-			this.getFromPoint = function() {
-				if (arguments.length > 2) {
-					return Audio.prototype.getFromPoint.apply(this, arguments);
-				}
-				var start = $.now();
-				var res = Audio.prototype.getFromPoint.apply(this, arguments);
-				sumTime += $.now() - start;
-				$('#stl_text').css('opacity', '1')
-					          .html('Avg search time: ' + Math.round(sumTime / ++count) + 'ms (' + count + ')');
-				return res;
-			}
-
-			return this;
-		},
-
-		getFromPoint: function(x, y, selector) {
-			if (selector === undefined) {
-				/* Call aggregate function for search of audio element
-				into search areas by mouse cursor position. */
-				return (function() {
-					for (var i in this.searchAreas) {
-						var elem = this.getFromPoint(x, y, this.searchAreas[i]);
-						if (elem !== null) {
-							return elem;
-						}
-					}
-					return null;
-				}).apply(this, arguments);
-			}
-
-			// get mouse cursor position relative to the element,
-			// -2 - left, -1 - above, 0 - inside, 1 - below, 2 - right.
-			var getRelPos = function(elem) {
-				// skip preloaded elements
-				if (!$(elem).filter(':visible').size()) {
-					return -1;
-				}
-
-				return $(elem).getRelativePosition(x, y);
-			}
-
-			// target elements
-			elements = $(selector).find('.audio');
-
-			// find element on mouse cursor position by dichotomy method
-			var b = 0, e = elements.size();
-			while (b != e) {
-				var i = parseInt((b + e) / 2);
-				var res = getRelPos(elements.get(i));
-				if (res > 0) {
-					b = i + 1;
-				}
-				else if (res < 0) {
-					e = i;
-				}
-				else {
-					return elements.get(i);
-				}
-			}
-			return null;
+			return $('#vkach');
 		}
 	};
 
-	var audio = (new Audio(
-		'#profile_audios',
-		'#page_wall_posts',
-		'#page_body'
-	)).click(function(e) {
-		// ignore clicks on elements except '.duration' element
-		if ($(this).find('.duration').getRelativePosition(e.pageX, e.pageY) !== 0) {
+	$(document).delegate('#header', 'click', function(e) {
+		if ($(e.target).closest('#vkach').size()) {
 			return;
 		}
 
-		alert(this.id);
+		panel.get().toggle();
+
+	}).delegate('.duration', 'click', function(e) {
+		var audio = $(this).closest('.audio');
+		if (!audio.size()) {
+			return;
+		}
+
+		panel.get().append(audio.clone());
 	});
-
-	$(document).click(function(e) {
-		// ignore clicks on elements except panel container element
-		var container = $('#header');
-		if (container.getRelativePosition(e.pageX, e.pageY) !== 0) {
-			return;
-		}
-
-		// get panel (create if it isn't exist)
-		var panel = $('#vkach');
-		if (!panel.size()) {
-			container.append('<div id="vkach" style="border-top: 1px dashed #D9E0E7; padding: 8px 0px 0px; margin: 8px 0px 0px;">VKACH</div>');
-			return;
-		}
-
-		if (!panel.filter(':visible').size() || panel.getRelativePosition(e.pageX, e.pageY) === -1) {
-			panel.toggle();
-		}
-	});
-
-	audio.debug(true);
 });
 
