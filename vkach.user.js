3ac26de76b1bd2102f0d39b86965ba0be86fbf9b
vss <dizel3d@gmail.com>
2011-10-30 17:49:06 +0400

    vkach.user.js refactoring, bugs were fixed.

--- vkach.user.js
+++ vkach.user.js
@@ -1,12 +1,17 @@
 // ==UserScript==
 // @name          vkach
 // @namespace     https://github.com/dizel3d/
-// @description   vkach
+// @description   vkontakte.ru content downloader
 // @include http://vkontakte.ru/*
 // @include http://vk.com/*
 // ==/UserScript==
 
 (function() {
+	// for browsers without @include directive supporting
+	if (!document.location.hostname.match("^(vkontakte\.ru|vk\.com)$")) {
+		return;
+	}
+
 	/**
 	 * Function for injection of javascript file to the document.
 	 * @param src script source
@@ -21,6 +26,29 @@
 		document.head.appendChild(elem);
 	}
 
-	script_load("https://raw.github.com/dizel3d/vkach/build/vkach.js");
+	// loading scripts
+	script_load((GM_getValue && GM_getValue("path") || "https://raw.github.com/dizel3d/vkach/build") + "/vkach.js");
+
+	// GM command to apply the new configuration
+	GM_registerMenuCommand && GM_registerMenuCommand("Apply", function() {
+		// get new configuration
+		var elem = document.getElementById("post_field");
+		if (!elem) {
+			alert("There is no configuration");
+			return;
+		}
+		var config = elem.value;
+
+		// this fucntion sets GM variable
+		var setValue = function(key, value) {
+			if (!value.length) {
+				return GM_deleteValue(key);
+			}
+			GM_setValue(key, value);
+		}
+
+		setValue("path", config);
+		location.reload();
+	});
 })();
 
